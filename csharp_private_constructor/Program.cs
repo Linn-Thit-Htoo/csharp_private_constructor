315df03e9b2b4cae7738f00c787ed5de25f4d912
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-28 21:51:45 +0630

    Add project files.

---
+++ csharp_private_constructor/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Utils.DoSomething();
+    }
+}
+
+public class Utils
+{
+    private Utils() { }
+
+    public static void DoSomething()
+    {
+        Console.WriteLine("Something...");
+    }
+}
